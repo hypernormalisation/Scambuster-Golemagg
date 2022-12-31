8eba047e78704b273999f67c811d68fbcaf824e3
hypernormalisation <52763122+hypernormalisation@users.noreply.github.com>
2022-12-31 20:22:22 +0000

    Framework implemented with a dummy list

---
+++ list.lua
@@ -0,0 +1,11 @@
+local _, t = ...
+t.case_table = {
+    [0] = {
+        name = "Thrall",
+        guid = "Player-GDSAKG-53295G",
+        category = "gdkp",
+        description = "Stole all the gold from a gdkp.",
+        url = "some_other_url",
+        aliases = {"Raegar"},
+    },
+}
--- settings.lua
+++ settings.lua
@@ -2,5 +2,6 @@ local _, t = ...
 t.addon_name = "Cutpurse-GolemaggEU"
 t.my_realm = "Golemagg"
 t.my_provider = "GolemaggEU Discord"
+t.my_name = "GolemaggEU Discord"
 t.my_description = "Cutpurse list of scammers curated by the Golemagg EU discord."
 t.my_url = "https://discord.gg/golemagg"
