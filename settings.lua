2b57b47e80266d221ac46da521774edebcbb6bf9
Ok <20994642+OkDruid@users.noreply.github.com>
2023-02-07 01:18:21 +0000

    provider name now discord.gg/golemagg

--- settings.lua
+++ settings.lua
@@ -1,6 +1,6 @@
 local _, t = ...
 t.my_name = "Golemagg Discord Blocklist"
-t.my_provider = "Golemagg Discord"
+t.my_provider = "https://discord.gg/golemagg"
 t.my_realm = "Golemagg"
-t.my_description = "Cutpurse list of scammers curated by the Golemagg discord."
+t.my_description = "A list of scammers curated by the golemagg discord community."
 t.my_url = "https://discord.gg/golemagg"
